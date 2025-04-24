8a6f79b0a4bc927498bfb156512100409ba8781e
Jerry Lee <oojerryleeoo@gmail.com>
2025-04-24 13:49:41 +0800

    refactor(scripts): update GNU stow installation script to better comply with shellcheck

--- scripts/install_stow.sh
+++ scripts/install_stow.sh
@@ -2,21 +2,21 @@
 # Installs the stow package to ~/.local/bin.
 # This is useful for installing stow on systems without root access.
 
-mkdir -p ${HOME}/.local/src
-cd ${HOME}/.local/src
+mkdir -p "${HOME}"/.local/src
+cd "${HOME}"/.local/src || exit
 
 # Download and extract the stow package
 wget http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
 tar -xvf stow-latest.tar.gz
 
-release=$(ls -Avr | grep -m1 -axEe 'stow-[0-9.]+')
-cd $release
+release=$(find . -maxdepth 1 -type d -name 'stow-[0-9.]*' | sort -V | tail -n 1)
+cd "$release" || exit
 
 # Configure and build the stow package
-./configure --prefix=${HOME}/.local/
+./configure --prefix="${HOME}"/.local/
 make
 make install
 
 # Clean up
-cd ${HOME}/.local/src
-rm -rf stow-latest.tar.gz ${release}*
+cd "${HOME}"/.local/src || exit
+rm -rf stow-latest.tar.gz "${release}"*
