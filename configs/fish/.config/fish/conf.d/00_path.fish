e8f0d2c2cb509ecd7352066517dd852f7a36a357
Jerry Lee <oojerryleeoo@gmail.com>
2025-05-04 16:49:09 +0800

    feat(fish): add path only when it exists

--- configs/fish/.config/fish/conf.d/00_path.fish
+++ configs/fish/.config/fish/conf.d/00_path.fish
@@ -1,2 +1,9 @@
-fish_add_path -g ~/.local/bin
-fish_add_path -g /opt/homebrew/bin
+function add_path_if_exists
+    if test -d $argv[1]
+        fish_add_path -g $argv[1]
+    end
+end
+
+add_path_if_exists $HOME/.local/bin
+add_path_if_exists $HOME/.cargo/bin
+add_path_if_exists /opt/homebrew/bin
