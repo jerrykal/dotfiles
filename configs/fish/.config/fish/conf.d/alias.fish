e5d2ecd894820df4fa955976f1fef044cb88e035
Jerry Lee <oojerryleeoo@gmail.com>
2025-09-16 14:08:45 +0800

    feat(fish): enhance alias configuration for batcat command

--- configs/fish/.config/fish/conf.d/alias.fish
+++ configs/fish/.config/fish/conf.d/alias.fish
@@ -4,7 +4,7 @@ if type -q eza
 end
 
 type -q bat; and alias cat="bat"
-type -q batcat; and alias cat="batcat"
+type -q batcat; and alias cat="batcat"; and alias bat="batcat"
 
 type -q fuck; and alias fk="fuck"
 
