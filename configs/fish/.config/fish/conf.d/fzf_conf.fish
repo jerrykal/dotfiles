10f5482a2bf69d01d9f08c6c16b5ac6cf7b79731
Kuei-An Lee <oojerryleeoo@gmail.com>
2025-11-26 20:59:46 +0800

    feat(fish,fzf): use rose-pine theme for both fish and fzf

--- configs/fish/.config/fish/conf.d/fzf_conf.fish
+++ configs/fish/.config/fish/conf.d/fzf_conf.fish
@@ -1,7 +1,7 @@
 set -g fzf_fd_opts --hidden
 set -gx FZF_DEFAULT_OPTS "\
-    --color=bg+:#363a4f,bg:,spinner:#f4dbd6,hl:#ed8796 \
-    --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
-    --color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
-    --color=selected-bg:#494d64 \
-    --color=border:#363a4f,label:#cad3f5"
+    --color=fg:#908caa,bg:#191724,hl:#ebbcba\
+    --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba\
+    --color=border:#403d52,header:#31748f,gutter:#191724\
+    --color=spinner:#f6c177,info:#9ccfd8\
+    --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
