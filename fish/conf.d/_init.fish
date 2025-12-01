93b4cc9ddecb2328f43b1732608d6de3905c029c
Kuei-An Lee <oojerryleeoo@gmail.com>
2025-12-01 18:44:51 +0800

    refactor(fish): small tweak

--- fish/conf.d/_init.fish
+++ fish/conf.d/_init.fish
@@ -1,15 +1,5 @@
 # Things here are loaded first
 
-# Set XDG basedirs.
-# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
-set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME $HOME/.local/share
-set -q XDG_STATE_HOME; or set -gx XDG_STATE_HOME $HOME/.local/state
-set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
-for xdgdir in (path filter -vd $XDG_CONFIG_HOME $XDG_DATA_HOME $XDG_STATE_HOME $XDG_CACHE_HOME)
-    mkdir -p $xdgdir
-end
-
 # --- Paths ---
 set -e fish_user_paths
 fish_add_path $HOME/.cargo/bin
