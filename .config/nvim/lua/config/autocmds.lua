85ab27da6b94e06ebb0effea5634010aac8c5d02
Kuei-An Lee <oojerryleeoo@gmail.com>
2026-04-10 17:03:33 +0800

    update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,12 +3,12 @@ local function augroup(name)
   return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
--- Trim trailing whitespace
-autocmd("BufWritePre", {
-  group = augroup("trim_trailing_whitespace"),
-  pattern = "*",
-  command = "%s/\\s\\+$//e",
-})
+-- -- Trim trailing whitespace
+-- autocmd("BufWritePre", {
+--   group = augroup("trim_trailing_whitespace"),
+--   pattern = "*",
+--   command = "%s/\\s\\+$//e",
+-- })
 
 -- Refresh statusline on event
 autocmd("DiagnosticChanged", {
