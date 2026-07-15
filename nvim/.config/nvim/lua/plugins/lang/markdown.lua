bd0ae1f15414807476fa45b114b70fa909704b4a
Kuei-An Lee <oojerryleeoo@gmail.com>
2026-07-15 18:09:12 +0800

    feat(nvim): disable markview.nvim under ghostty quick terminal

--- nvim/.config/nvim/lua/plugins/lang/markdown.lua
+++ nvim/.config/nvim/lua/plugins/lang/markdown.lua
@@ -3,6 +3,7 @@ return {
   {
     "OXY2DEV/markview.nvim",
     lazy = false, -- markview.nvim lazy loads itself
+    enabled = vim.env.GHOSTTY_QUICK_TERMINAL == nil, -- plain text in the quick-terminal scratchpad
     opts = {
       preview = {
         hybrid_modes = { "n" },
