066d908be52df30ecc302bae34d9cf8741357766
Kuei-An Lee <oojerryleeoo@gmail.com>
2026-07-21 09:54:46 +0800

    fix(fish): switch to vi key bindings with insert-mode defaults

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-    # Add this to disable fzf shift-tab completion
-    bind shift-tab complete-and-search
-    bind -M insert shift-tab complete-and-search
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+    set --no-event -g fish_key_bindings fish_vi_key_bindings
 end
