396c8841c1198169d0b07f323326bec8cf2b52a6
Kuei-An Lee <oojerryleeoo@gmail.com>
2026-07-26 23:42:09 +0800

    fix(nvim): disable markview rendering inside codediff views

--- nvim/.config/nvim/lua/plugins/lang/markdown.lua
+++ nvim/.config/nvim/lua/plugins/lang/markdown.lua
@@ -4,6 +4,41 @@ return {
     "OXY2DEV/markview.nvim",
     lazy = false, -- markview.nvim lazy loads itself
     opts = {},
+    config = function(_, opts)
+      require("markview").setup(opts)
+
+      -- Disable markview rendering inside codediff views; restore on close
+      local codediff_disabled = {}
+
+      local function disable_in_tab()
+        local commands = require("markview.commands")
+        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.bo[buf].filetype == "markdown" and not codediff_disabled[buf] then
+            commands.disable(buf)
+            codediff_disabled[buf] = true
+          end
+        end
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = { "CodeDiffOpen", "CodeDiffFileSelect" },
+        callback = vim.schedule_wrap(disable_in_tab),
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "CodeDiffClose",
+        callback = function()
+          local commands = require("markview.commands")
+          for buf in pairs(codediff_disabled) do
+            if vim.api.nvim_buf_is_valid(buf) then
+              commands.enable(buf)
+            end
+          end
+          codediff_disabled = {}
+        end,
+      })
+    end,
   },
 
   {
