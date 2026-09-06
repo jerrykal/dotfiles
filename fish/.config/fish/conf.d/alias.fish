3b586c39fb44441d6bba2b4fc845c5e4d856ab95
Kuei-An Lee <oojerryleeoo@gmail.com>
2026-09-06 11:55:10 +0800

    feat(fish): add mise-update alias

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -3,6 +3,8 @@ type -q batcat; and alias cat="batcat"; and alias bat="batcat"
 
 type -q autossh; and alias ssh="autossh -M 0"
 
+type -q mise; and alias mise-update="mise self-update -y && mise up"
+
 alias mkdir="mkdir -p"
 
 alias t="~/.tmux/scripts/session-picker.sh"
